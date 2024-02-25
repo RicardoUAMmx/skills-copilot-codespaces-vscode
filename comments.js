d3c9b261d0464c277fed69ede5071ba16d233b85
Ricardo Hdz. <90495063+RicardoUAMmx@users.noreply.github.com>
2024-02-25 08:21:26 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,65 +1,30 @@
 // Create Web Server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
+// Create a Web Server that listens for requests on port 3000. When requests are received, the server will send back a response with the following content:
+// "Hello, World!"
 
-var template = {
-  html: function (title, list, body, control) {
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-    `;
-  },
-  list: function (filelist) {
-    var list = '<ul>';
-    var i = 0;
-    while (i < filelist.length) {
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
-    }
-    list = list + '</ul>';
-    return list;
-  }
-}
+// Use the createServer method of the http module to create the server.
 
-var app = http.createServer(function (request, response) {
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var pathname = url.parse(_url, true).pathname;
+// The createServer method takes a callback function that accepts two arguments, request and response.
 
-  if (pathname === '/') {
-    if (queryData.id === undefined) {
-      fs.readdir('./data', function (error, filelist) {
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var list = template.list(filelist);
-        var html = template.html(title, list,
-          `<h2>${title}</h2>${description}`,
-          `<a href="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
-    } else {
-      fs.readdir('./data', function (error, filelist) {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-          var title = queryData.id;
-          var list = template.list(filelist);
-          var html = template.html(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>
-             <a href="/update?id=${title}">update</a>
-             <form action="delete_process" method="post">
-               <input type="hidden" name="id" value="${title}">
+// The request object contains information about the incoming request such as headers, URL, and more.
+
+// The response object is used to send data back to the client that made the request.
+
+// Use the writeHead method of the response object to set the status code and content type in the response header.
+
+// Use the end method of the response object to send the response.
+
+// The server should listen for requests on port 3000.
+
+// You can start the server by running node comments.js in the terminal. You can then make requests to the server using a web browser, Postman, or another tool of your choice.
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!');
+});
+
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
